7a241406ff4be0c3bbf725ad66ae01cca3c15ca8
Vadagon <l3adagon@gmail.com>
2018-02-12 14:02:45 +0200

    Removed

--- the extension/assets/popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('tho').addEventListener("click", ()=>{
-	chrome.runtime.sendMessage({path: document.getElementById('the').value}, function(response) {
-	  console.log(12);
-	});
-});
